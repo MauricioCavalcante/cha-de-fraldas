5ecd130e9214ebb6fb533847ae0fb84ffdfda500
Mauricio Cavalcante <mauricio.cavalcante@globalhitss.com.br>
2024-11-18 17:49:24 -0300

    RSVP e Gallery

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light custom-navbar-bg sticky-top">
       <div className="container">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/cha-de-fraldas">
           Chá de Fraldas
         </Link>
         <button
@@ -23,7 +23,7 @@ function Header() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">
+              <Link className="nav-link" to="/cha-de-fraldas">
                 Início
               </Link>
             </li>
---
+++ src/pages/Gallery.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Gallery() {
+    return (
+      <div className="container custom-container text-center mt-5">
+        <div className="container-fluid justify-content-center col-12 col-md-7 mt-5">
+          <p className="lead">Estamos trabalhando nessa seção que trará todas as fotos do evento.</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Gallery;
